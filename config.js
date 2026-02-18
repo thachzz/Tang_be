66c4f7246656e0233abe09199124e03155fb8cec
thachzz <81088445+thachzz@users.noreply.github.com>
2026-02-18 23:27:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    introDesc: `Đường đời trăm ngả vấn vương,
+     Sao em lạc bước chung đường cùng anh?
+     Tym này vốn dĩ mong manh
+     Em vừa ghé nhẹ... hóa thành yêu thương`,
+    btnIntro: 'iu iu',
+    title: 'Sol có thương Adi không nè 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnYes: 'Bé yêu mỗi anh <33',
+    btnNo: 'Không, Mơ đi Anh trai à :3',
+    question:'Sao lại yêu anh vậy bé?',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Vì anh xinh...🥰  .Bé yêu yêu yêu anh thật nhiều, nhiều nắm <33333333 <33333333',
+    mess: 'Anh biết mà 🥰. nhưng kệ em cơ 😘😘',
+    messDesc: 'Bé chậpp của anh, tới với anh nào😙.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/Ant.mixc/'
 }
